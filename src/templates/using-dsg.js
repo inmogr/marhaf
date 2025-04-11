c8c66756ccc6c8c963b0be3cfab5050c2c4b5005
Morhaf Shamia <inmogr@msn.com>
2025-04-11 21:43:01 +0400

    deleted import

--- src/templates/using-dsg.js
+++ src/templates/using-dsg.js
@@ -3,7 +3,6 @@ import * as React from "react";
 import { Link } from "gatsby";
 
 import Layout from "../components/layout";
-import Seo from "../components/seo";
 
 const UsingDSG = () => (
     <Layout>
@@ -18,7 +17,5 @@ const UsingDSG = () => (
         <Link to="/">Go back to the homepage</Link>
     </Layout>
 );
-
-export const Head = () => <Seo title="Using DSG" />;
 
 export default UsingDSG;
